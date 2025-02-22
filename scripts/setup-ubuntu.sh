02099b21490e8799e3ac8a702e3fd26867668580
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-02-22 12:36:36 -0600

    docs(ubuntu): update ubuntu

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -15,6 +15,11 @@ rm -f packages.microsoft.gpg
 sudo apt update
 sudo apt install -y code
 
+sudo apt install gnome-shell-extensions
+sudo apt install gnome-tweaks
+sudo apt install gnome-browser-connector
+sudo apt uninstall apt remove update-notifier  # this is because we want to stop update notification popups
+
 # Install NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -25,12 +30,14 @@ nvm install --lts
 nvm use --lts
 
 # Install Git
-sudo apt install -y git
+sudo apt install -y git gh
 
 # Install Git LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install -y git-lfs
 git lfs install
+
+git config --global user.email dickerson.sd@gmail.com
 
 # Install Nginx
 sudo apt install -y nginx
